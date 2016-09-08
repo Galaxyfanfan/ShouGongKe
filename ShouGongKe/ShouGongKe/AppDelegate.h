ac29e2afa262a8bd3cef584ad51fb2e7e0b7ee36
Galaxy <852347376@qq.com>
2016-09-08 22:26:48 +0800

    添加整体目录框架

---
+++ ShouGongKe/ShouGongKe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShouGongKe
+//
+//  Created by Galaxy on 16/8/25.
+//  Copyright © 2016年 Galaxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
